a64b7616e4fb59d822610ce722199cc2cc0599c0
Srajan Jain <46938701+srajan2627@users.noreply.github.com>
2026-04-04 19:34:36 -0500

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parantheses = { '[':']',
+                        '{':'}',
+                        '(':')'}
+
+        p_stack = []
+
+        for char in s:
+            if char in parantheses.keys():
+                p_stack.append(char)
+            else:
+                p_open = p_stack.pop()
+                if char == parantheses[p_open]:
+                    return True
+        
+        return False
+            
