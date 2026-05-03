61b0203686bb5395ba5a8459c6a04115d702bba6
Srajan Jain <46938701+srajan2627@users.noreply.github.com>
2026-05-02 21:05:00 -0500

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l = 0
+        r = 0
+        largest = 0
+        ans = 0
+
+        while r < len(nums) - 1:
+            for i in range(l,r+1):
+                largest = max(largest,i + nums[i])
+            l = r + 1
+            
+            ans += 1
+        
+        return ans
