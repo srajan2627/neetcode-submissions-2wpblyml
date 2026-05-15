5792578fddbb289be43819af342e0c3128b6d4f2
Srajan Jain <46938701+srajan2627@users.noreply.github.com>
2026-05-15 06:02:38 -0500

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_dict = defaultdict(int)
+        t_dict = defaultdict(int)
+
+
+        for char in s:
+            s_dict[char] += 1
+
+        for char in t:
+            t_dict[char] += 1
+        
+        return s_dict == t_dict
