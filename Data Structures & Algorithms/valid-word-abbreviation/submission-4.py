f43e492d964e2ff7fad5cd119d025f946aff1826
Srajan Jain <46938701+srajan2627@users.noreply.github.com>
2026-05-25 05:08:12 -0500

    Add: valid-word-abbreviation - submission-4

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        i = 0
+        j = 0
+        char = ''
+
+        while i < len(word) and j < len(abbr):
+            if word[i] == abbr[j]:
+                i += 1
+                j += 1
+
+            elif abbr[j].isalpha():
+                return False
+            
+            elif abbr[j].isdigit():
+                char += abbr[j]
+                
+                j += 1
+                i += int(char)
+
+        return i == len(word) and j == len(abbr)
