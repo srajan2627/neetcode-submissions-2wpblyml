cef926a0ecaba31b4e1aedca1ac441f10fb895b6
Srajan Jain <46938701+srajan2627@users.noreply.github.com>
2026-06-24 11:05:08 +0530

    Add: find-pivot-index - submission-1

---
+++ Data Structures & Algorithms/find-pivot-index/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        prefix = [0]
+
+        for i in range(len(nums)):
+            prefix.append(prefix[-1]+nums[i])
+
+        for i in range(len(nums)):
+            left_sec = prefix[i]
+            right_sec = prefix[-1] - prefix[i+1]
+            if left_sec == right_sec:
+                return i 
+
+        return -1
