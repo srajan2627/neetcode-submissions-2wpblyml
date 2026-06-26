2b0a9e8492bcbe3a812d509cba49eea2019205d7
Srajan Jain <46938701+srajan2627@users.noreply.github.com>
2026-06-26 20:01:32 +0530

    Add: max-consecutive-ones-iii - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones-iii/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left, right = 0, 0
+        count = 0
+        ans = 0
+
+        while right < len(nums):
+            if nums[right] == 0:
+                count += 1
+
+            while count > k:
+                if nums[left] == 0:
+                    count -= 1      
+
+                left += 1       
+
+            ans = max(ans,right-left+1)
+            right += 1  
+                 
+        return ans
+                
+
+
+
+
+
+            
+
